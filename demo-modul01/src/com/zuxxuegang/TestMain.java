bbccadcc0ed7a04de983c654ebfb213145aa68fa
zuxuegang126 <zuxuegang2022@126.com>
2022-12-16 22:01:54 +0800

    第次修改三

--- demo-modul01/src/com/zuxxuegang/TestMain.java
+++
@@ -1,9 +0,0 @@
-package com.zuxxuegang;
-
-public class TestMain {
-    public static void main(String[] args) {
-        System.out.println(123456);
-        System.out.println("git file>");
-        System.out.println("git blame<");
-    }
-}
